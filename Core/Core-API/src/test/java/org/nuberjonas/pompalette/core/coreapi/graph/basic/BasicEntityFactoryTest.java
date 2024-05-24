c3d79ad6121c839138c4985ff3952a5b07cabfa6
Jonas Nuber <nuber-jonas@web.de>
2024-05-24 03:47:50 +0200

    POMP-81: Added Basic Factories unit tests
    Took 10 minutes

---
+++ Core/Core-API/src/test/java/org/nuberjonas/pompalette/core/coreapi/graph/basic/BasicEntityFactoryTest.java
@@ -0,0 +1,20 @@
+package org.nuberjonas.pompalette.core.coreapi.graph.basic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BasicEntityFactoryTest {
+
+    @Test
+    void createEntity_ShouldReturnEntityWithCorrectData(){
+        var entityFactory = new BasicEntityFactory<String, String>();
+        var expected = "data";
+
+        var entity = entityFactory.createEntity(expected);
+
+        assertThat(entity.getData()).isEqualTo(expected);
+        assertThat(entity.getRelationships()).isEmpty();
+    }
+
+}
---
+++ Core/Core-API/src/test/java/org/nuberjonas/pompalette/core/coreapi/graph/basic/BasicRelationshipFactoryTest.java
@@ -0,0 +1,22 @@
+package org.nuberjonas.pompalette.core.coreapi.graph.basic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BasicRelationshipFactoryTest extends BaseTest {
+
+    @Test
+    void createRelationship_ShouldReturnRelationshipWithCorrectSourceAndDestinationAndData(){
+        var relationshipFactory = new BasicRelationshipFactory<String, String>();
+        var sourceEntity = createBasicEntity("source");
+        var destinationEntity = createBasicEntity("destination");
+        var data = "data";
+
+        var relationship = relationshipFactory.createRelationship(sourceEntity, destinationEntity, data);
+
+        assertThat(relationship.source()).isEqualTo(sourceEntity);
+        assertThat(relationship.destination()).isEqualTo(destinationEntity);
+        assertThat(relationship.data()).isEqualTo(data);
+    }
+}
